fb3512d81bcb041c5d0eee4424cc6537dfe4c67d
Himynameismarco <marcozander94@web.de>
2022-11-25 13:10:19 +0100

    Start Journey Button opens alert

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 console.log("Hello world!");
 
+const startJourneyButton = document.querySelector('#StartJourneyButton');
+startJourneyButton.onclick = () => alert("Suuuuuper! ... also fast ... Die App ist leider noch nicht fertig. Aber schick mir gerne ein Mail :)");
